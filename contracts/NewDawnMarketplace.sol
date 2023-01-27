efb9da26fdd47ec182d70ff7a42cb5ba6e97e756
Sheeeev66 <roi.hakim@gmail.com>
2023-01-27 11:07:09 +0200

    add new dawn marketplace contract

--- contracts/NewDawnMarketplace.sol
+++ contracts/NewDawnMarketplace.sol
@@ -176,6 +176,8 @@ library ECDSA {
     function toTypedDataHash(bytes32 domainSeparator, bytes32 structHash) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
     }
+}
 
+contract NewDawnMarketplace {
     
 }
